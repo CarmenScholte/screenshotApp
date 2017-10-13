5e2dc97274db84045fa7dad43e7130dc41c5c064
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2017-10-13 14:55:24 +0200

    Refactor ScreenshotApp

--- src/main/java/csl/Main.java
+++ src/main/java/csl/Main.java
@@ -2,19 +2,35 @@ package csl;
 
 import csl.model.ScreenCaptureService;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String strPath = "C:/Users/Carmen/dev/screenshots";
-//        Path path = Paths.get("C:/Users/Carmen/dev/screenshots");
+        // Make screenshot from certain page
+        // Save it under certain file name
+        // To a certain location
 
         ScreenCaptureService scs = new ScreenCaptureService();
-//        scs.makeScreenshot("Capture", "jpg", strPath);
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Carmen\\bin\\chromedriver");
-        ChromeDriver webDriver = new ChromeDriver();
-        webDriver.get("http://google.com");
+
+        String url = "carmenscholte.github.io/carmenscholteart";
+        String fileName = "BrowserShot";
+        String fileLocation = "C:/Users/Carmen/dev/screenshots";
+
+        scs.makeBrowserScreenshot(url, fileName, fileLocation);
+
+        // Make screenshot from desktop
+        // Save it under certain file name
+        // To a certain location
+
+        fileName = "DesktopShot";
+        scs.makeDesktopScreenshot(fileName, fileLocation);
+
+
     }
 }
--- src/main/java/csl/model/ScreenCaptureService.java
+++ src/main/java/csl/model/ScreenCaptureService.java
@@ -1,10 +1,15 @@
 package csl.model;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
 import java.util.logging.Logger;
@@ -16,32 +21,55 @@ public class ScreenCaptureService {
 
     private static final Logger LOG = Logger.getLogger("ScreenCaptureService");
 
-    public void makeScreenshot(String fileName) {
+    public void makeDesktopScreenshotSaveToRoot(String fileName) {
         FileSystem fs = FileSystems.getDefault();
         Iterable<Path> paths = fs.getRootDirectories();
         Iterator<Path> it = paths.iterator();
-        makeScreenshot(fileName, "png", it.next());
+        makeDesktopScreenshot(fileName, it.next().toString());
     }
 
-    public void makeScreenshot(String fileName, String fileType, String path) {
-        Path p = Paths.get(path);
-        makeScreenshot(fileName, fileType, p);
+    public void makeDesktopScreenshot(String fileName, String fileLocation) {
+        makeDesktopScreenshot(fileName, "png" ,fileLocation);
     }
 
-    public void makeScreenshot(String fileName, String fileType, Path path) {
+    public void makeDesktopScreenshot(String fileName, String fileType, String fileLocation) {
         try {
             Robot robot = new Robot();
             Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
             BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
             ByteArrayOutputStream out = new ByteArrayOutputStream();
-            ImageIO.write(screenFullImage, "png", out);
+            ImageIO.write(screenFullImage, fileType, out);
 
-            File directory = new File(path.toString());
+            File directory = new File(fileLocation);
             System.out.println(directory.mkdirs());
-
             Files.write(Paths.get(directory.toString() + "/" + fileName + formatFileType(fileType)), out.toByteArray());
-        } catch (Exception ex) {
+        } catch (AWTException | IOException ex) {
             LOG.info(ex.getMessage());
+        }
+    }
+
+    public void makeBrowserScreenshot(String url, String fileName, String fileLocation) {
+        Path path = Paths.get(fileLocation);
+        ChromeOptions options = new ChromeOptions().addArguments("--start-fullscreen");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://" + url);
+        makeBrowserScreenshot(fileName, driver, path);
+        driver.close();
+    }
+
+    public void makeBrowserScreenshot(String fileName, ChromeDriver driver, Path locationPath) {
+        makeBrowserScreenshot(fileName, driver, locationPath.toString());
+    }
+
+    public void makeBrowserScreenshot(String fileName, ChromeDriver driver, String locationPath) {
+        byte[] screenshot = driver.getScreenshotAs(OutputType.BYTES);
+        File targetDir = new File(locationPath);
+        System.out.println(targetDir.mkdirs());
+
+        try {
+            Files.write(Paths.get(targetDir.toString() + "/" + fileName + ".png"), screenshot);
+        } catch (IOException e) {
+            LOG.info(e.getMessage());
         }
     }
 
